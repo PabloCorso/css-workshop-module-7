8ec4961620af97c8a8e72c2fb311321232df437f
Pablo Corso <pabcorso@gmail.com>
2022-06-12 14:22:39 +0200

    1 - Header

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components/macro";
 import format from "date-fns/format";
 
+import { QUERIES } from "../../constants";
+
 const Logo = (props) => {
   return (
     <Wrapper>
@@ -20,6 +22,10 @@ const Wrapper = styled.div`
 const Link = styled.a`
   font-family: var(--font-family-logo);
   font-size: 3rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: 4rem;
+  }
 `;
 
 const TodaysDate = styled.p`
