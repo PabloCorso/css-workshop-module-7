0e6f460fa8e85d69b384e782eb1d86daf790718a
Pablo Corso <pabcorso@gmail.com>
2022-06-13 14:19:24 +0200

    2 - Flex Opinion avatars

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components/macro";
 
+import { QUERIES } from "../../constants";
+
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
     <a href={`/story/${id}`}>
@@ -17,6 +19,12 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: column;
+  }
 `;
 
 const Avatar = styled.img`
@@ -25,6 +33,11 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-left: auto;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-left: revert;
+  }
 `;
 
 const AuthorName = styled.p`
